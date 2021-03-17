a0bfe0160eaf0885d2ab3887f266834519f78309
DesireeJoy <desireebradish@gmail.com>
2021-03-17 18:02:19 -0400

    Fix Routes

--- routes/cards.js
+++ routes/cards.js
@@ -1,8 +1,17 @@
 const router = require("express").Router();
-const cards = require("../data/cards.json");
+const path = require("path");
+const fs = require("fs").promises;
+
+const dataPath = path.join(__dirname, "../data/cards.json"); // joining the path segments to create an absolute path
 
 router.get("/", (req, res) => {
-  res.json(cards);
+  fs.readFile(dataPath, { encoding: "utf8" })
+    .then((data) => {
+      res.status(200).send(data);
+    })
+    .catch(() => {
+      res.status(500).send({ message: "File not found" });
+    });
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -2,12 +2,10 @@ const router = require("express").Router();
 const path = require("path");
 const fs = require("fs").promises;
 
-const users = require("../data/users");
-
 const dataPath = path.join(__dirname, "../data/users.json"); // joining the path segments to create an absolute path
 
 router.get("/", (req, res) => {
-  fs.readFile(dataPath, "utf8")
+  fs.readFile(dataPath, { encoding: "utf8" })
     .then((data) => {
       res.status(200).send(data);
     })
@@ -17,17 +15,18 @@ router.get("/", (req, res) => {
 });
 
 router.get("/:id", (req, res) => {
-  fs.readFile(dataPath, "utf8")
+  fs.readFile(dataPath, { encoding: "utf8" })
     .then((data) => {
-      const selectedUser = users.find((user) => user._id === req.params.id);
+      const newData = JSON.parse(data);
+      const selectedUser = newData.find((user) => user._id === req.params.id);
       if (!selectedUser) {
         res.status(404).send({ message: "User ID not found" });
       } else {
-        res.send(selectedUser);
+        res.send({ selectedUser });
       }
     })
-    .catch(() => {
-      res.status(500).send({ message: "File not found" });
+    .catch((err) => {
+      console.log(err);
     });
 });
 
