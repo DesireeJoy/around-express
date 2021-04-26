04470fd980567ee08d9c113f9b7a303278566752
DesireeJoy <desireebradish@gmail.com>
2021-04-25 20:17:01 -0400

    Fix eslint errors

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 
 const mongoose = require("mongoose");
-var bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
+const helmet = require("helmet");
 
 mongoose.connect("mongodb://localhost:27017/aroundb", {
   useNewUrlParser: true,
@@ -9,10 +10,6 @@ mongoose.connect("mongodb://localhost:27017/aroundb", {
   useFindAndModify: false,
   useUnifiedTopology: true,
 });
-var db = mongoose.connection;
-
-//Bind connection to error event (to get notification of connection errors)
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 const usersRouter = require("./routes/users");
 const cardsRouter = require("./routes/cards");
@@ -20,7 +17,7 @@ const cardsRouter = require("./routes/cards");
 // listen to port 3000
 const { PORT = 3000 } = process.env;
 const app = express();
-var helmet = require("helmet");
+
 app.use(helmet());
 app.use(bodyParser.json());
 app.use((req, res, next) => {
@@ -36,6 +33,13 @@ app.get("*", (req, res) => {
   res.status(404).send({ message: "Requested resource not found" });
 });
 
+app.post("*", (req, res) => {
+  res.status(404).send({ message: "Requested resource not found" });
+});
+
+app.delete("*", (req, res) => {
+  res.status(404).send({ message: "Requested resource not found" });
+});
 app.listen(PORT, () => {
   // if everything works fine, the console will show which port the application is listening to
 });
